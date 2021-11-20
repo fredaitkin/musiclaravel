5a75502868bb831370dd149f3e142e88896811b3
Melissa Aitkin <melissa.aitkin@jacksonriver.com>
2021-11-20 10:45:19 -0500

    do not add comma at end of categories

--- public/js/word_cloud.js
+++ public/js/word_cloud.js
@@ -86,8 +86,6 @@ $(document).ready(function() {
               terms.pop();
               // add the selected item
               terms.push( ui.item.value );
-              // add placeholder to get the comma-and-space at the end
-              terms.push( "" );
               this.value = terms.join( ", " );
               // Remove from future search
               removeFromAvailableCategories(ui.item.id);
