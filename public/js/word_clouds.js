250c5a7335ea1fff6313711b7976c0ca670ec4ca
Melissa Aitkin <melissa.aitkin@jacksonriver.com>
2021-12-03 13:52:47 -0500

    display whitespace in lyrics

--- public/js/word_clouds.js
+++ public/js/word_clouds.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
             form += '<div>' +
                 '<div style="width:50%;float:left" id="' + song.id + '" class="song">' + song.song.substr(0, 50) + '</div>' +
                 '<div style="width:50%;float:left">' + song.artist + '</div>' +
-                '<div id="modal-' + song.id + '" style="display:none;">' + song.lyrics + '</div>' +
+                '<div id="modal-' + song.id + '" style="display:none;white-space:pre;">' + song.lyrics + '</br>gday</br>there</div>' +
                 '</div>';
         });
 
@@ -52,8 +52,8 @@ $(document).ready(function() {
             $('#' + modal_div).dialog({
                 modal : true ,
                 title: $(this).text(),
-                height : 500,
-                width : 600,
+                height : 700,
+                width : 400,
                 open : function() {
                     $("[aria-describedby=" + modal_div + "]").addClass('ui-dialog-lyrics');
                 },
