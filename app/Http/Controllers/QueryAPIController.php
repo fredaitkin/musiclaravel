48d37f9e1c2029df1a825a3d0a9600601ea9f160
Melissa Aitkin <melissa.aitkin@jacksonriver.com>
2022-04-04 21:09:03 -0400

    add first predefined query - search words

--- app/Http/Controllers/QueryAPIController.php
+++ app/Http/Controllers/QueryAPIController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Music\Song\Song;
 use DB;
 use Exception;
 use Illuminate\Http\Request;
@@ -29,7 +30,12 @@ class QueryAPIController extends Controller
         ];
         if (isset($request->predefined_query)):
             $predefined_query = $request->predefined_query;
-            $results = "ID:1 "." TODO";
+            $myquery = $request->myquery;
+            switch($predefined_query):
+                case 1:
+                    $results = $this->wordsInSongs($request->myquery);
+                    break;
+            endswitch;
         elseif (isset($request->myquery)):
             $myquery = $request->myquery;
             try {
@@ -60,6 +66,31 @@ class QueryAPIController extends Controller
             'results' => $results,
             'show_cols' => $show_cols,
         ]);
+    }
+
+    private function wordsInSongs($words)
+    {
+        $results = '';
+        $songs = Song::select('id', 'title', 'lyrics')->where('lyrics', 'like', '%' . $words . '%')->get();
+        foreach($songs as $song):
+            $pos = stripos($song->lyrics, $words);
+            if ($pos > 50):
+                $pos -= 50;
+            endif;
+            $phrase = substr($song->lyrics, $pos, 150);
+            $phrase = str_replace(["\r\n", "\n", "\\"], [' ', ' ', ''], $phrase);
+            if (stripos($phrase, $words) !== 0) {
+                // Strip start to get whole word
+                $start_pos = strpos($phrase, " ");
+                $phrase = substr($phrase, $start_pos);
+            }
+            // Strip end to get whole word.
+            $end_pos = strrpos($phrase, " ");
+            $phrase = substr($phrase, 0, $end_pos);
+            // @todo Improve presentation.
+            $results .= str_pad($song->id, 6) . ' ' . str_pad(substr($song->title, 0, 20), 30, ".") . ' ' . trim($phrase) . "\n";
+        endforeach;
+        return 'Count: ' . count($songs) . "\n\n" . $results;
     }
 
 }
