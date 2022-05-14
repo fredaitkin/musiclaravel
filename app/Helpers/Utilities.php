fc4980723b9ad1c227a5dc4ab71489a2013c3e31
Melissa Aitkin <melissa.aitkin@jacksonriver.com>
2022-05-14 16:38:36 -0400

    remove unnecessary files and functions

--- app/Helpers/Utilities.php
+++ app/Helpers/Utilities.php
@@ -30,30 +30,6 @@ function getCountryNames()
     return Cache::store('redis')->get('country_names');
 }
 
-function executeCurlRequest($url) {
-    $curl = curl_init();
-
-    curl_setopt_array($curl, [
-        CURLOPT_URL => $url,
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_ENCODING => "",
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 30,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => "GET",
-    ]);
-
-    $response = curl_exec($curl);
-    $err = curl_error($curl);
-
-    if ($err):
-        throw new Exception($err);
-    endif;
-
-    return json_decode($response);
-}
-
 /**
  * Replaces characters file systems cannot process
  */
