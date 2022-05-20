67cf7d49a90b69a6df256e454d2ac2085cab7a7d
Melissa Aitkin <melissa.aitkin@jacksonriver.com>
2022-05-19 21:14:55 -0400

    more functionality for ffmpeg

--- app/Console/Commands/ConvertAppleMusic.php
+++ app/Console/Commands/ConvertAppleMusic.php
@@ -13,7 +13,7 @@ class ConvertAppleMusic extends Command
      * @var string
      */
     protected $signature = 'ffmpeg:convert
-                            {--song= : The song}
+                            {--song= : The song with directory}
                             {--dir= : The song directory}';
 
     /**
@@ -44,27 +44,37 @@ class ConvertAppleMusic extends Command
 
             $options = $this->options();
 
-            if (! isset($options['dir'])):
-                $this->error('Directory is required');
+            $return = shell_exec(sprintf("which %s", escapeshellarg('ffmpeg')));
+            if (! $return):
+                $this->error('The command ffmpeg does not exist or is not configured on your system');
                 exit;
             endif;
 
-            if (! is_dir($options['dir'])):
-                $this->error('Invalid diretory path');
-                exit;
-            endif;
-
-            $files = array();
-            foreach (scandir($options['dir']) as $file):
-                if ($file !== '.' && $file !== '..' && $file != '.DS_Store'):
-                    if (strpos($file, '.mp4') !== false):
-                        $this->info($file);
-                        $new_file = str_replace('.mp4', '.mp3', $file);
-                        $command = 'ffmpeg -i "' . $options['dir'] . '/' . $file . '" "' . $options['dir'] . '/' . $new_file . '"';
-                        exec($command);
-                    endif;
+            if (isset($options['song'])):
+                if (strpos($options['song'], '.mp4') !== false):
+                    $new_file = str_replace('.mp4', '.mp3', $options['song']);
+                    $command = 'ffmpeg -i "' . $options['song'] . '" "' . $new_file . '"';
+                    exec($command);
                 endif;
-            endforeach;
+            endif;
+
+            if (isset($options['dir'])):
+                if (! is_dir($options['dir'])):
+                    $this->error('Invalid diretory path');
+                    exit;
+                endif;
+
+                foreach (scandir($options['dir']) as $file):
+                    if ($file !== '.' && $file !== '..' && $file != '.DS_Store'):
+                        if (strpos($file, '.mp4') !== false):
+                            $this->info($file);
+                            $new_file = str_replace('.mp4', '.mp3', $file);
+                            $command = 'ffmpeg -i "' . $options['dir'] . '/' . $file . '" "' . $options['dir'] . '/' . $new_file . '"';
+                            exec($command);
+                        endif;
+                    endif;
+                endforeach;
+            endif;
 
             $this->info('The songs have been converted.');
         } catch (Exception $e) {
