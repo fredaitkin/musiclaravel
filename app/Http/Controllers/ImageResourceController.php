9eca829d6271cf9615433bdb697a23024f92db4f
Melissa Aitkin <melissa.aitkin@jacksonriver.com>
2022-06-15 15:31:29 -0400

    add phpcs and phpcbf against the image controller

--- app/Http/Controllers/ImageResourceController.php
+++ app/Http/Controllers/ImageResourceController.php
@@ -26,31 +26,32 @@ class ImageResourceController extends Controller
         $this->song = $song;
     }
 
-    public function coverArt($id) {
-        if (! Cache::store('redis')->has('song_photo_' . $id)):
+    public function coverArt($id)
+    {
+        if (! Cache::store('redis')->has('song_photo_' . $id)) :
             $song = $this->song->get($id);
             $location = explode('/', $song->location);
             // Handle MAC and Win directory structures.
-            if (count($location) < 2):
+            if (count($location) < 2) :
                 $location = explode('\\', $song->location);
             endif;
             $location = config('filesystems.media_directory') . $location[0] . DIRECTORY_SEPARATOR . $location[1];
             $files = Storage::disk(config('filesystems.partition'))->files($location);
             $path = null;
-            if (count($files) > 0):
+            if (count($files) > 0) :
                 foreach ($files as $file):
-                    if (strpos($file, 'Large.jpg')):
+                    if (strpos($file, 'Large.jpg')) :
                         $path = config('filesystems.disks')[config('filesystems.partition')]['root'] . $file;
                         Cache::store('redis')->put('song_photo_' . $id, $path, 86400);
                         break;
                     endif;
-                 endforeach;
-             endif;
+                endforeach;
+            endif;
         else:
             $path = Cache::store('redis')->get('song_photo_' . $id);
         endif;
 
-        if (! $path):
+        if (! $path) :
             $path = Storage::disk('public')->path('black.jpeg');
         endif;
         return Response::download($path);
