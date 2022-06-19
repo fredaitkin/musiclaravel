0a853a7eba752e3c70340e540afb56363751dbf2
Melissa Aitkin <melissa.aitkin@jacksonriver.com>
2022-06-19 14:23:05 -0400

    code style for middleware

--- app/Http/Middleware/CheckAdmin.php
+++ app/Http/Middleware/CheckAdmin.php
@@ -1,17 +1,28 @@
 <?php
 
+/**
+ * Authentication Middleware
+ *
+ * @package Jukebox
+ * @author  Melissa Aitkin
+ */
+
 namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Auth;
 
+/**
+ * CheckAdmin filters requests to verify the user is the administrator.
+ */
 class CheckAdmin
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request Request object
+     * @param \Closure                 $next    Next function in the flow.
+     *
      * @return mixed
      */
     public function handle($request, Closure $next)
