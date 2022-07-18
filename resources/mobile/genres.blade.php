5619c8ed0a6f6f56bbf2e46bed4ebb3b0f297b98
Melissa Aitkin <melissa.aitkin@jacksonriver.com>
2022-07-18 12:54:31 -0400

    add mobile form for genres

---
+++ resources/mobile/genres.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="panel-body">
+
+        @if ($errors->any())
+            <div class="alert alert-danger" role="alert">
+                Please fix the following errors
+            </div>
+        @endif
+
+        @if (isset($message))
+            <p>{{ $message }}</p>
+        @endif
+
+        <div class="col-sm-3">
+            <h5>Genres</h5>
+        </div>
+
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <div class="row">
+                    <div class="col">
+                        <table class="table table-striped mysounds-table">
+
+                            <thead>
+                                <th>Genre</th>
+                                <th>&nbsp;</th>
+                            </thead>
+
+                            <tbody>
+                                @foreach ($genres as $genre)
+                                    <tr class="mysounds-tr">
+                                        <td class="table-text">
+                                            <div name="genre-title">{{ $genre->genre }}</div>
+                                        </td>
+                                        <td>
+                                            {{ csrf_field() }}
+                                            <a href="#" name="play_genre">play</a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                        {{ $genres->withPath('songs?genres=true')->links() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+
+@endsection
