685087a25306e67b8c59e81ae5391428a37fce89
Melissa Aitkin <melissa.aitkin@jacksonriver.com>
2022-07-19 11:12:20 -0400

    mobile version of word cloud forms

---
+++ resources/mobile/word.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="panel-body mysound-submit-form-div">
+
+        @include('common.errors')
+
+        <form action="/word-cloud" method="POST" class="form-horizontal">
+            {{ csrf_field() }}
+
+            <div class="col-md-3 pb-2">
+                <label for="word" class="control-label">Word</label>
+                <div class="form-inline">
+                    <input type="text" name="word" id="word" class="form-control" @if ( ! empty($word_cloud->word)) value="{{ $word_cloud->word }}" @endif>
+                </div>
+            </div>
+
+           <div class="col-sm-3 pb-2">
+                <button type="button" id="capitalize" class="btn btn-secondary">Capitalize</button>
+           </div>
+
+            <div class="col-sm-3 pb-2">
+                <label for="is_word" class="control-label">Is Word</label>
+                <div>
+                    <input type="checkbox" name="is_word" id="is_word" @if (!empty($word_cloud->is_word) && ($word_cloud->is_word)) checked @endif>
+                </div>
+            </div>
+
+            <div class="col-md-5 pb-2">
+                <label for="categories" class="control-label">Categories</label>
+                <div class="form-inline">
+                    <select class="categories form-control w-50" multiple="multiple" name="categories[]" id="categories"></select>
+                    <label class="pl-1">Name</label><input type="checkbox" name="set_name" id="set_name"/>
+                </div>
+            </div>
+
+            <div class="col-sm-3 pb-2">
+                <label for="variant_of" class="control-label">Variant of</label>
+                <div>
+                    <input type="text" name="variant" id="variant" class="form-control" @if ( ! empty($word_cloud->variant)) value="{{ $word_cloud->variant }}" @endif>
+                    <input type="hidden" name="variant_of" id="variant_of" @if ( ! empty($word_cloud->variant_of)) value="{{ $word_cloud->variant_of }}" @endif>
+                </div>
+            </div>
+
+            <div class="col-sm-3 pt-5">
+                <input type="hidden" name="id" id="song-id" value="{{ $word_cloud->id }}">
+                <input type="hidden" name="page" id="page" value="{{ $page }}">
+                <button type="submit" class="btn btn-primary">Update</button>
+                <input type="hidden" name="categories_json" id="categories_json" value="{{ $categories }}">
+                <a href="{{ url()->previous() }}" class="btn btn-primary">Back</a>
+            </div>
+
+        </form>
+
+    </div>
+@endsection
+
+@section('scripts')
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
+    <script src="{{ asset('js/category.js') }}"></script>
+    <script src="{{ asset('js/word_cloud_edit.js') }}"></script>
+@endsection
+
+@section('styles')
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
+@endsection
---
+++ resources/mobile/word_cloud.blade.php
@@ -0,0 +1,93 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="panel-body">
+
+        @if ($errors->any())
+            <div class="alert alert-danger" role="alert">
+                Please fix the following errors
+            </div>
+        @endif
+
+        @if (isset($message))
+            <div class="alert alert-warning ml-3 w-25" role="alert">
+                {{ $message }}
+            </div>
+        @endif
+
+        <div class="col-sm-3">
+            <h5>Word Cloud</h5>
+        </div>
+
+        <div class="panel panel-default">
+
+            <div class="panel-body">
+                <form class="form-inline" method="GET">
+                  <div class="form-group ml-3 mb-2">
+                    <label for="filter" class="col-sm-3 col-form-label">Filter</label>
+                    <input type="text" class="form-control" id="filter" name="filter" placeholder="Word/Category" value="{{ $filter }}">
+                  </div>
+                  <button type="submit" class="btn btn-primary">
+                    <i class="fa fa-search"></i>
+                  </button>
+                </form>
+
+                <table class="table table-striped mysounds-table">
+
+                    <thead>
+                        <th scope='col' class='sortable'>@sortablelink('word')</th>
+                        <th scope='col' class='sortable'>@sortablelink('category')</th>
+                        <th scope='col' class='sortable'>@sortablelink('count')</th>
+                        <th></th>
+                        <th></th>
+                     </thead>
+
+                    <tbody>
+                        @foreach ($word_cloud as $word)
+                            <tr class="mysounds-tr">
+                                <td class="table-text">
+                                    <div>
+                                        {{ csrf_field() }}
+                                        <a href="/word-cloud/{{ $word->id }}?page={{ $word_cloud->currentPage() }}">{{ $word->word }}</a>
+                                    </div>
+                                </td></div>
+                                </td>
+                                <td class="table-text">
+                                    <div>{{ $word->category_display }}</div>
+                                </td>
+                                <td class="table-text">
+                                    <div>{{ $word->count }}</div>
+                                </td>
+                                <td>
+                                   <input type="button" class="btn btn-link btn-mysounds" name="songs" id="songs-{{ $word->id }}" value="songs">
+                                </td>
+                                <td>
+                                    @if ($word->is_word)
+                                        <input type="button" class="btn btn-link btn-mysounds" name="dictionary" id="dictionary-{{ $word->word }}" value="dictionary">
+                                    @endif
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                  <div class="form-inline">
+                    {{ $word_cloud->appends(['filter' => $filter])->links() }}
+                     <form class="form-inline" method="GET">
+                        <input type="text" class="form-control" id="page" name="page" size=10>
+                        <input type="submit" class="btn btn-secondary" id="go" value="Go">
+                    </form>
+                </div>
+            </div>
+        </div>
+
+@endsection
+
+@section('scripts')
+    <script src="{{ asset('js/dictionary.js') }}"></script>
+    <script src="{{ asset('js/word_cloud.js') }}"></script>
+@endsection
+
+@section('styles')
+    <link href="{{ asset('css/word-cloud.css') }}" rel="stylesheet">
+@endsection
